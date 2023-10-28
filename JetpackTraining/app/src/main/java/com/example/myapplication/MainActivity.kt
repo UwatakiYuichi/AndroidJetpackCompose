b624a495416caec2a0e128cfc81fa882068c6248
UwatakiYuichi <fatehotaru@gmail.com>
2023-10-29 02:02:35 +0900

    mutableStateOfの動作試験

--- JetpackTraining/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ JetpackTraining/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -13,21 +14,35 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.Role.Companion.Button
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.myapplication.ui.theme.MyApplicationTheme
 
 
 class MainActivity : ComponentActivity() {
+
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -90,12 +105,83 @@ private fun searchValue(target:Int ,tbl:IntArray): Int {
 @Composable
 private fun MyApp(modifier: Modifier = Modifier) {
     Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-        DrawImageSample()
-        drawColum()
-        drawFooter()
+        rootDraw()
     }
 }
 
+@Composable
+fun rootDraw() {
+    var text by remember { mutableStateOf("") }
+    var name = remember {
+        mutableStateOf("パイパイ")
+    }
+    val polarBear = remember { mutableStateListOf<Int>() }
+
+    DrawImageSample()
+    drawColum()
+//    drawFooter()
+    drawFooterChange(name)
+    drawButton()
+
+    changeText(name)
+    drawName(name)
+
+}
+
+@Composable
+fun drawName(name: MutableState<String>) {
+    Text(text = name.value)
+}
+
+var num = 234
+
+@Composable
+fun drawCnt() {
+
+    Text(text = "$num")
+}
+
+@Composable
+fun drawName(name: String) {
+    Text(text = name)
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun drawInput() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.TopCenter
+    ) {
+        var text by remember { mutableStateOf("") }
+        OutlinedTextField(
+            value = text,
+            onValueChange = { text = it },
+            modifier = Modifier.padding(20.dp),
+            label = { Text(text = "Label") }
+        )
+    }
+}
+
+@Composable
+fun drawButton() {
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        var count by remember { mutableStateOf(0) }
+        Text(
+            text = "Tap count: $count",
+            modifier = Modifier.padding(20.dp)
+        )
+        Button(
+            onClick = { count++ }
+        ) {
+            Text(text = "Count up!")
+        }
+    }
+}
 
 @Composable
 fun drawColum() {
@@ -108,9 +194,6 @@ fun drawColum() {
 
 @Composable
 fun drawFooter() {
-
-
-
     Row(
         modifier = Modifier.fillMaxSize(),
         horizontalArrangement = Arrangement.SpaceBetween,
@@ -119,8 +202,11 @@ fun drawFooter() {
         Image(
             painter = painterResource(R.drawable.noel),
             contentDescription = "ノエちゃん",
-            modifier = Modifier.border(1.dp, Color.Green),
-            contentScale = ContentScale.Fit
+            modifier = Modifier
+                .border(1.dp, Color.Green)
+                .clickable { Log.d("TAG", "ノエちゃん") },
+            contentScale = ContentScale.Fit,
+
         )
         Image(
             painter = painterResource(R.drawable.noel),
@@ -145,6 +231,54 @@ fun drawFooter() {
 }
 
 @Composable
+fun drawFooterChange(name: MutableState<String>) {
+    Row(
+        modifier = Modifier.fillMaxSize(),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.Bottom
+    ){
+        Image(
+            painter = painterResource(R.drawable.noel),
+            contentDescription = "ノエちゃん",
+            modifier = Modifier
+                .border(1.dp, Color.Green)
+                .clickable { Log.d("TAG", "ノエちゃん")
+                    name.value = "ノエちゃん"
+                           },
+            contentScale = ContentScale.Fit,
+
+            )
+        Image(
+            painter = painterResource(R.drawable.noel),
+            contentDescription = "ノエちゃん",
+            modifier = Modifier.border(1.dp, Color.Blue).clickable { Log.d("TAG", "ノエちゃん")
+                name.value = "マリン"
+            },
+            contentScale = ContentScale.Fit
+        )
+        Image(
+            painter = painterResource(R.drawable.noel),
+            contentDescription = "ノエちゃん",
+            modifier = Modifier.border(1.dp, Color.Yellow),
+            contentScale = ContentScale.Fit
+        )
+
+        Image(
+            painter = painterResource(R.drawable.noel),
+            contentDescription = "ノエちゃん",
+            modifier = Modifier.border(1.dp, Color.Red),
+            contentScale = ContentScale.Fit
+        )
+    }
+}
+
+
+
+private fun changeText(message: MutableState<String>) {
+    message.value = "world from Compose"
+}
+
+@Composable
 fun drawBox() {
    Box {
        Image(
@@ -164,15 +298,12 @@ fun drawBox() {
 
 @Composable
 fun DrawImageSample(){
-
     Image(
         painter = painterResource(R.drawable.noel),
         contentDescription = "ノエちゃん",
         modifier = Modifier.border(1.dp, Color.Red),
         contentScale = ContentScale.Fit
     )
-
-
 }
 
 @Composable
