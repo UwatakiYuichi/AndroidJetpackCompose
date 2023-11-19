36448d5e9f021324c07005c089fab11be1fe9400
UwatakiYuichi <fatehotaru@gmail.com>
2023-11-20 00:53:47 +0900

    NavControllerを追加中

--- JetpackTraining/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ JetpackTraining/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,42 +1,24 @@
 package com.example.myapplication
 
-import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.offset
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.semantics.Role.Companion.Button
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.core.content.ContextCompat.startActivity
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.myapplication.ui.theme.MyApplicationTheme
 
 
@@ -44,10 +26,6 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
-
-
-
 
             MyApplicationTheme {
 
@@ -85,29 +63,29 @@ private fun Sample() {
     }
 }
 
-private fun GetValue(no:Int) : Int{
-    return no
-}
-
-/**
- *
- */
-private fun searchValue(target:Int ,tbl:IntArray): Int {
-    for (idx in 0 .. tbl.size) {
-        if(tbl[idx] == target) {
-            return idx
-        }
-    }
-
-    return -1
-}
-
 @Composable
 private fun MyApp(modifier: Modifier = Modifier) {
     Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-        rootDraw()
+        val navController = rememberNavController()
+
+        NavHost(navController = navController, startDestination = "screen1") {
+            composable(route = "screen1") {
+                Screen1(onClickButton = { navController.navigate("screen2") })
+            }
+            composable(route = "screen2") {
+                Screen2(onClickButton = { navController.navigateUp() })
+            }
+        }
+
+
+
+        //rootDraw()
     }
 }
+
+
+
+
 
 @Composable
 fun rootDraw() {
